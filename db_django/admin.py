4cb188734ceab29143f4babdc05fdc3cf55cbcde
dev-farhadi <buiss.farhadi.g@gmail.com>
2024-01-07 16:14:36 +0330

    set views and urls

--- db_django/admin.py
+++ db_django/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Teacher
+from .models import Courses
 
-# Register your models here.
+admin.site.register(Courses)
+admin.site.register(Teacher)
--- db_django/urls.py
+++ db_django/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("teacher/<int:name_text>", views.teacher, name="teacher")
 ]
