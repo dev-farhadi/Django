7bef5a638e8e0eec3f72ac4741bac28fad74494d
dev-farhadi <buiss.farhadi.g@gmail.com>
2024-01-08 10:52:29 +0330

    create table Teacher_courses add to admin and insert some values

--- db_django/admin.py
+++ db_django/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Teacher
 from .models import Courses
+from .models import Teacher_courses
 
+admin.site.register(Teacher_courses)
 admin.site.register(Courses)
 admin.site.register(Teacher)
--- db_django/models.py
+++ db_django/models.py
@@ -5,3 +5,7 @@ class Teacher(models.Model):
 
 class Courses(models.Model):
     name_text = models.CharField(max_length=100)
+
+class Teacher_courses(models.Model):
+    teacher_id = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    courses_id = models.ForeignKey(Courses, on_delete=models.CASCADE)
