b13f4e1dc629b19846a64522c2878d8c21373855
dev-farhadi <buiss.farhadi.g@gmail.com>
2024-01-08 12:10:46 +0330

    complete

--- db_django/urls.py
+++ db_django/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("teacher/", views.teacher, name="teacher"),
-    path("courses/", views.courses, name="courses")
+    path("courses/", views.courses, name="courses"),
+    path("teacher_courses/<int:teacher_id>", views.teacher_courses, name="teacher_courses")
 ]
--- db_django/views.py
+++ db_django/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from .models import Teacher
 from django.template import loader
 from .models import Courses
+from .models import Teacher_courses
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -23,4 +24,14 @@ def courses(request):
     }
 
     return HttpResponse(template.render(context, request))
+
+def teacher_courses(request, teacher_id):
+    
+  #teacher_id = 2 # Provide the desired teacher ID
+
+  course_names = Courses.objects.filter(teacher_courses__teacher_id_id=teacher_id).values_list('name_text', flat=True)
+  response_str = ', '.join(course_names)
+
+  return HttpResponse(response_str)
+
 
